eab8a1cd5bb1254b64a18cc6544c74f7c75e1eed
Matt <slaterduskinwilson@gmail.com>
2024-06-18 00:09:14 +0200

    restyled svelte version

---
+++ NSA_JK/nsa/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+
+    import Nav from "./Nav.svelte";
+
+
+</script>
+
+<Nav />
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+
+@import '../../static/system.css';
+
+</style>
---
+++ NSA_JK/nsa/src/routes/Nav.svelte
@@ -0,0 +1,126 @@
+<div>
+	<ul role="menu-bar">
+		<li role="menu-item" tabindex="0" aria-haspopup="true">
+			File
+			<ul role="menu">
+				<li role="menu-item">
+					<a href="https://nsajk.bandcamp.com/" target="_blank" rel="noopener noreferrer">Bio</a>
+				</li>
+
+				<li role="menu-item">
+					<a href="./video.html">Video</a>
+				</li>
+				<li role="menu-item">
+					<a href="https://nsajk.bandcamp.com/" target="_blank" rel="noopener noreferrer"
+						>Buy Our Music on BandCamp</a
+					>
+				</li>
+				<li role="menu-item">
+					<a href="https://ko-fi.com/matthewwilson69" target="_blank" rel="noopener noreferrer"
+						>Donate Money Without Buying Any Music</a
+					>
+				</li>
+				<li role="menu-item" class="divider">
+					<a href="friends.html">Friends of the Band</a>
+				</li>
+			</ul>
+		</li>
+		<li role="menu-item" tabindex="0" aria-haspopup="true">
+			Edit
+			<ul role="menu">
+				<li role="menu-item">
+					<a href="/"
+						>NSA jk is a deconstructionist pop music project by Matt Wilson. Please enjoy our
+						website! Be sure to also check the File and Special menus for the most up to date and
+						valuable information.</a
+					>
+				</li>
+			</ul>
+		</li>
+		<li role="menu-item" tabindex="0" aria-haspopup="true">
+			Side Projects
+			<ul role="menu">
+				<li role="menu-item">
+					<a href="https://skannieraitt.bandcamp.com" target="_blank" rel="noopener noreferrer"
+						>Skannie Raitt</a
+					>
+				</li>
+				<li role="menu-item">
+					<a
+						href="https://soundcloud.com/hardmikeandthelemonades/calm-down-10"
+						target="_blank"
+						rel="noopener noreferrer">Hard Mike and the Lemonades</a
+					>
+				</li>
+			</ul>
+		</li>
+		<li role="menu-item" tabindex="0" aria-haspopup="true">
+			SVU
+			<ul role="menu">
+				<li role="menu-item">
+					<a
+						href="https://you-son-of-a-b.itch.io/svuroleplay"
+						target="_blank"
+						rel="noopener noreferrer">Law and Order: Special Victims Unit: The Roleplaying Game</a
+					>
+				</li>
+				<li role="menu-item">
+					<a href="https://svuroleplay.com"
+						>Listen to
+						<em>SVU: The RPG: The Soundtrack</em></a
+					>
+				</li>
+				<li role="menu-item">
+					<a href="https://svuroleplay.com">The Making of SVU: The RPG</a>
+				</li>
+				<li role="menu-item">
+					<a href="https://svuroleplay.com"
+						>11 Facts You Never Would Have Guessed About Law and Order: Special Victims Unit</a
+					>
+				</li>
+			</ul>
+		</li>
+
+		<li role="menu-item" tabindex="0" aria-haspopup="true">
+			Special
+			<ul role="menu">
+				<li role="menu-item">
+					<a href="https://slaterduskinwilson.medium.com/" target="_blank" rel="noopener noreferrer"
+						>Essays And Press</a
+					>
+				</li>
+
+				<li role="menu-item">
+					<a href="art.html">Sketchbook</a>
+				</li>
+				<li class="close-hidden-menu" role="menu-item">
+					<a href="#menu">Shut Down</a>
+				</li>
+			</ul>
+		</li>
+	</ul>
+</div>
+
+<style>
+	body,
+	html {
+		margin: 0;
+		padding: 0;
+	}
+
+	ul {
+		list-style: none;
+	}
+
+	.window h1,
+	.window h2,
+	.window h3,
+	.window p {
+		padding: 2rem;
+	}
+
+	ul li {
+		margin: 1.2rem;
+		font-size: 1.2rem;
+	}
+</style>
