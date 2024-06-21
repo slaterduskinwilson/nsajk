d38516b83250ebda6deffd0c154423f7f342f296
Matt <slaterduskinwilson@gmail.com>
2024-06-22 00:33:01 +0200

    why wont the svgs in the static/icon folder render?

---
+++ NSA_JK/nsa/src/routes/sideprojects/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+	// your script goes here
+</script>
+
+<div class="alert-box outer-border scale-down" style="width:30rem;">
+	<div class="inner-border">
+		<div class="alert-contents" style="padding-left: 30px; padding-right:20px;">
+			<section class="field-row" style="justify-content: flex-start">
+				<div class="square">69</div>
+				<p class="alert-text" style="padding-left:10px;">
+					<slot></slot>
+				</p>
+			</section>
+			<section class="field-row" style="justify-content: flex-end">
+				<button class="btn">Fuck This Shit</button>
+				<button class="btn btn-default" style="width:95px;">Check It Out</button>
+			</section>
+		</div>
+	</div>
+</div>
+
+<!-- markup (zero or more items) goes here -->
+
+<style>
+    
+</style>
---
+++ NSA_JK/nsa/src/routes/sideprojects/skannieraitt/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    // your script goes here
+</script>
+
+
+Skannie Raitt is a Bonnie Raitt ska cover band.
+<style>
+    /* your styles go here */
+</style>
+
+<!-- markup (zero or more items) goes here -->
