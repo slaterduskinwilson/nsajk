3f26987904f1a61c3a2dd9037beb24ce7987fa27
Matt <slaterduskinwilson@gmail.com>
2024-06-21 23:25:35 +0200

    now the front page displays a random message each time

--- NSA_JK/nsa/src/routes/+page.svelte
+++ NSA_JK/nsa/src/routes/+page.svelte
@@ -1,9 +1,45 @@
 <script>
+	const awfulPeople = [
+		'H.H. Holmes',
+		'Mike Love',
+		'Carlos Mencia',
+		'Bernie Madoff',
+		'Paul Ryan',
+		'Bristol Palin',
+		'David Koresh'
+	];
+
+    const adverbs = [
+        'second',
+        'third',
+        'fourth',
+        'sixteenth',
+        'eighth',
+        'eleventh'
+    ]
+
+    
+	// Function to get a random element from the array
+	function getRandomPerson() {
+		const randomIndex = Math.floor(Math.random() * awfulPeople.length);
+		return awfulPeople[randomIndex];
+	}
+
+    function getRandomAdverb() {
+        const randomIndex = Math.floor(Math.random() * adverbs.length)
+        return adverbs[randomIndex]
+    }
+
+	// Assign a random person to a variable
+	let awfulPerson = getRandomPerson();
+
+    let adverb = getRandomAdverb()
+
 </script>
 
 <div class="standard-dialog center scale-down" style="width:30rem;">
-	<h1 class="dialog-text">"NSA/jk is my favorite band!"</h1>
-	<p class="dialog-text">-H.H. Holmes</p>
+	<h1 class="dialog-text">"NSA/jk is my {adverb} favorite band!"</h1>
+	<p class="dialog-text">-{awfulPerson}</p>
 </div>
 
 <style>
