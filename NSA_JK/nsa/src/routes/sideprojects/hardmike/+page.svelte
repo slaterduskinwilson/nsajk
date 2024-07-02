81a83c7cd05d46baa830aef9dc62657dc8e207e2
Matt <slaterduskinwilson@gmail.com>
2024-07-02 22:12:15 +0200

    more words

--- NSA_JK/nsa/src/routes/sideprojects/hardmike/+page.svelte
+++ NSA_JK/nsa/src/routes/sideprojects/hardmike/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	// this is to store the sideproject's url, so i can pass it to the layout for this page
-	//and render a link 
+	//and render a link
 	import { urlStore } from '$lib/stores';
 
 	// Set the URL value
@@ -11,7 +11,12 @@
 <div class="square" style="border: 1px solid black;">
 	<img src="/02.png" alt="Bonnie Raitt, but ska" style="max-width: 100%;" />
 </div>
-<p class="alert-text" style="padding-left:2rem;">Hard Mike and the Lemonades is an improvisational collective that records every time they're in a room together. The band has changed their name several times.</p>
+<p class="alert-text" style="padding-left:2rem;">
+	Hard Mike and the Lemonades is an improvisational collective that records every time they're in a
+	room together. In this way they're kind of like the Stasi, but more irie. Based out of Chapel Hill, the band has changed
+	its name several times, and have also been billed as Solo Basura, Ice/No Ice, Calm Down, Blues
+	Lawyer, and Miami Face Eater.
+</p>
 
 <!-- markup (zero or more items) goes here -->
 
--- NSA_JK/nsa/src/routes/sketchbook/+page.svelte
+++ NSA_JK/nsa/src/routes/sketchbook/+page.svelte
@@ -1,9 +1,44 @@
 <script>
-    // your script goes here
+	import { fly } from 'svelte/transition';
+
+	let showWindow = true;
 </script>
-<h1>FUCK YO ASS THIS IS SKETCHBOOK!</h1>
-<style>
-    /* your styles go here */
-</style>
+
+{#if showWindow}
+	<div class="window sketchbook" transition:fly={{ x: -100, y: 800, duration: 600 }}>
+		<div class="title-bar">
+			<button aria-label="Close" class="close" on:click={() => (showWindow = false)}></button>
+			<h1 class="title">Sketchbook</h1>
+			<!-- <button aria-label="Resize" class="resize"></button> -->
+		</div>
+		<div class="separator"></div>
+
+		<div class="window-pane">
+			<h4>This section showcases the raw materials used to build NSA jk.</h4>
+			<p>
+				Music, particularly the obsessively multitracked kind, doesn't usually queaph itself into
+				existence one day just because you happen to be at an artist's retreat on Crete that day.
+				Time must be taken to collect many grains of sand that irritate you. It is these tiny grains
+				of irritant that will gradually form the pearl that will hopefully get you laid or at least
+				some free alcohol or drugs.
+			</p>
+			<p>
+				When they're doing a good job, members of NSA/jk are always taping, and almost always paying
+				attention. There are hundreds of iPhone voice memo recordings that paint as complete a
+				picture as you can of a person trying not to just the fucking fucking aaaaaaaaaaaa. And
+				sometimes even succeeding.
+			</p>
+
+            <a href="">Click here to visit NSAjk.org, home of the archival recordings of NSA jk.</a>
+		</div>
+	</div>
+{/if}
 
 <!-- markup (zero or more items) goes here -->
+
+<style>
+	.sketchbook {
+		max-width: 60%;
+		margin: 8rem;
+	}
+</style>
