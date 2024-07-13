852fa3c160032a8eaaa2c285bf2c2d0d694d4d56
Matt <slaterduskinwilson@gmail.com>
2024-07-13 14:01:56 +0200

    tryna exclude all the images

---
+++ NSA_JK/nsa/src/routes/gallery/+page.svelte
@@ -0,0 +1,88 @@
+<script>
+	// import { fly } from 'svelte/transition';
+
+	// State variables for videos
+	// let videos = [
+	// 	{
+	// 		id: 1,
+	// 		title: 'My House',
+	// 		src: 'https://player.vimeo.com/video/748428315?badge=0&autopause=0&player_id=0&app_id=58479',
+	// 		isLarge: false,
+	// 		show: true,
+	// 		direction: { x: -300, y: -500 }
+	// 	},
+	// 	{
+	// 		id: 2,
+	// 		title: 'Law and Order SVU Theme',
+	// 		src: 'https://www.youtube.com/embed/OQpZDAT_6iw?si=GAvZIv90d6aBIvPv',
+	// 		isLarge: false,
+	// 		show: true,
+	// 		direction: { x: 1200, y: 1200 }
+	// 	},
+	// 	{
+	// 		id: 3,
+	// 		title: 'Conversation',
+	// 		src: 'https://www.youtube.com/embed/U1lJdEw6ElQ?si=vitJOEFw9NPgGYrO',
+	// 		isLarge: false,
+	// 		show: true,
+	// 		direction: { x: 1000, y: 0 }
+	// 	}
+	// ];
+
+	// function toggleShow(id) {
+	// 	const video = videos.find((v) => v.id === id);
+	// 	video.show = !video.show;
+	// 	videos = [...videos];
+	// }
+
+	// function toggleSize(id) {
+	// 	const video = videos.find((v) => v.id === id);
+	// 	video.isLarge = !video.isLarge;
+	// 	videos = [...videos];
+	// }
+</script>
+
+<div class="container">
+	
+			<div
+				class="window"
+				
+				
+			>
+				<div class="title-bar">
+					<button on:click={() => console.log("fucking clicked it on closed")} aria-label="Close" class="close"></button>
+					<h1 class="title">Butt Butt Butt</h1>
+					<button on:click={console.log('fucking clicked it on resize')} aria-label="Resize" class="resize"></button>
+				</div>
+				<div class="separator"></div>
+				<div class="window-pane">
+					<p>fuck yo ass</p>
+
+                    <enhanced:img src="/static/02.png" alt="oh fuck"/>
+				</div>
+			</div>
+		
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: row;
+		gap: 6rem;
+		align-items: flex-start;
+		flex-wrap: wrap;
+	}
+
+	.window {
+		max-width: max-content;
+		max-height: min-content;
+	}
+
+	/* .hidden {
+		display: none;
+	} */
+
+	.enlarged {
+		min-width: 60vw;
+	}
+</style>
--- NSA_JK/nsa/vite.config.js
+++ NSA_JK/nsa/vite.config.js
@@ -1,6 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+//  https://kit.svelte.dev/docs/images
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [enhancedImages(),
+	sveltekit()]
 });
